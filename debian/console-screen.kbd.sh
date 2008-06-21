neverware-mirrors/kbd

commit 87be6f1232aa0f8ec9719c772958e9014a65bd1c

Set /sys/module/vt/parameters/default_utf8 at bootup, closes: #478480
This parameter decides whether newly opened VTs should enter Unicode mode
automatically.  It is available in recent versions of the Linux kernel.

This also changes console-screen.kbd.sh to ask for the charmap only once
instead of one time for every VT to be initialized.

debian/console-screen.kbd.sh
@@ -51,6 +51,13 @@ else
     DEVICE_PREFIX="/dev/tty"
 fi
 
+# determine the system charmap
+CHARMAP=`LANG=$LANG LC_ALL=$LC_ALL LC_CTYPE=$LC_CTYPE locale charmap 2>/dev/null`
+if [ "$CHARMAP" = "UTF-8" -a -z "`eval echo \$CONSOLE_MAP\$CONSOLE_MAP_vc$vc`" ]
+then
+    UNICODE_MODE=yes
+fi
+
 reset_vga_palette ()
 {
     if [ -f /proc/fb ]; then
@@ -71,8 +78,7 @@ unicode_start_stop ()
             eval $var=$value
         done
     fi
-    CHARMAP=`LANG=$LANG LC_ALL=$LC_ALL LC_CTYPE=$LC_CTYPE locale charmap 2>/dev/null`
-    if [ "$CHARMAP" = "UTF-8" -a -z "$(eval echo \$CONSOLE_MAP\$CONSOLE_MAP_vc$vc)" ]; then
+    if [ -n "$UNICODE_MODE" ]; then
         action=unicode_start
     else
         action=unicode_stop
@@ -137,6 +143,14 @@ setup ()
         [ "$VERBOSE" != "no" ] && log_action_end_msg 0
     fi
 
+    # Default to Unicode mode for new VTs?
+    if [ -f /sys/module/vt/parameters/default_utf8 ]; then
+        if [ -n "$UNICODE_MODE" ]; then
+            echo 1
+        else
+            echo 0
+        fi > /sys/module/vt/parameters/default_utf8
+    fi
 
     # Per-VC font+sfm
     PERVC_FONTS="`set | grep "^CONSOLE_FONT_vc[0-9]*="  | tr -d \' `"

commit 2b51f0469b4a457d57333c89dabcb296362a9217

Read /etc/environment or /etc/default/locale earlier, closes: #486787
The problem described in the bug was introduced in commit
87be6f1232aa0f8ec9719c772958e9014a65bd1c where the locale check was
moved earlier to avoid checking for unicode mode twice.

Thanks to Stanislav Maslovski <stanislav.maslovski@gmail.com> for the
patch.

debian/console-screen.kbd.sh
@@ -51,6 +51,16 @@ else
     DEVICE_PREFIX="/dev/tty"
 fi
 
+# read environment settings
+if [ -f /etc/environment ] || [ -f /etc/default/locale ]
+then
+for var in LANG LC_CTYPE LC_ALL
+do
+    value=$(egrep "^[^#]*${var}=" /etc/environment /etc/default/locale 2>/dev/null | tail -n1 | cut -d= -f2)
+    eval $var=$value
+done
+fi
+
 # determine the system charmap
 CHARMAP=`LANG=$LANG LC_ALL=$LC_ALL LC_CTYPE=$LC_CTYPE locale charmap 2>/dev/null`
 if [ "$CHARMAP" = "UTF-8" -a -z "$CONSOLE_MAP" ]
@@ -70,15 +80,7 @@ reset_vga_palette ()
 unicode_start_stop ()
 {
     vc=$1
-    if [ -f /etc/environment ] || [ -f /etc/default/locale ]
-    then
-        for var in LANG LC_CTYPE LC_ALL
-        do
-            value=$(egrep "^[^#]*${var}=" /etc/environment /etc/default/locale 2>/dev/null | tail -n1 | cut -d= -f2)
-            eval $var=$value
-        done
-    fi
-    if [ -n "$UNICODE_MODE" -a -z "`eval echo \$CONSOLE_MAP_vc$vc`" ]; then
+    if [ -n "$UNICODE_MODE" -a -z "`eval echo \\$CONSOLE_MAP_vc$vc`" ]; then
         action=unicode_start
     else
         action=unicode_stop